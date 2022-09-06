baae291053d5d1c8f3c3cda94221c41268a70433
Nathan Pena <nnpena@outlook.com>
2022-09-05 20:57:03 -0500

    Add delete

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -10,7 +10,8 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {
+        public class Query : IRequest<List<Activity>> 
+        {
             
         }
 
